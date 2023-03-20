25a717501d8447854a7a2e00fb0dca9ec91251e5
difagume <difagume@gmail.com>
2023-03-19 22:14:46 -0500

    Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,13 @@
+import Navbar from '@/components/Navbar'
 import useCurrentUser from '@/hooks/useCurrentUser'
 import { NextPageContext, type NextPage } from 'next'
-import { getSession, signOut } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
 	// const hello = api.example.hello.useQuery({ text: 'from tRPC' })
 	const { data: user } = useCurrentUser()
 
-	return (
-		<>
-			<h1 className='text-4xl text-white'>Netflix Clone</h1>
-			<p className='text-white'>Logged in as: {user?.email}</p>
-
-			<button className='h-10 w-full bg-white' onClick={() => signOut()}>
-				Logout
-			</button>
-		</>
-	)
+	return <Navbar />
 }
 
 export async function getServerSideProps(context: NextPageContext) {
