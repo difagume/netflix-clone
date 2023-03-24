4ec2be29017c14552d6decc3105693b102adc3c2
difagume <difagume@gmail.com>
2023-03-24 10:17:49 -0500

    clean env sample

--- src/env.mjs
+++ src/env.mjs
@@ -19,10 +19,8 @@ const server = z.object({
 		(str) => process.env.VERCEL_URL ?? str,
 		// VERCEL_URL doesn't include `https` so it cant be validated as a URL
 		process.env.VERCEL ? z.string().min(1) : z.string().url()
-	),
+	)
 	// Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-	DISCORD_CLIENT_ID: z.string(),
-	DISCORD_CLIENT_SECRET: z.string()
 })
 
 /**
@@ -48,9 +46,7 @@ const processEnv = {
 	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 	NEXTAUTH_JWT_SECRET: process.env.NEXTAUTH_JWT_SECRET,
-	NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-	DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-	DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET
+	NEXTAUTH_URL: process.env.NEXTAUTH_URL
 	// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 }
 
