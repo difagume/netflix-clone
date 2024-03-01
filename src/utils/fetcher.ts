e58a9edbee25b4f09a469debc546f4feefb3ffd2
difagume <difagume@gmail.com>
2024-02-29 20:11:51 -0500

    sitio base para mantenimiento

--- src/utils/fetcher.ts
+++
@@ -1,7 +0,0 @@
-const fetcher = async (url: string) => {
-	const response = await fetch(url)
-	const data = await response.json()
-	return data
-}
-
-export default fetcher
--- src/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export const setNew = (fecha: string) => {
-	const fechaLanzamiento = new Date(fecha)
-	const fechaActual = new Date()
-	const tiempoTranscurrido = fechaActual.getTime() - fechaLanzamiento.getTime()
-	const mesesTranscurridos = tiempoTranscurrido / (1000 * 60 * 60 * 24 * 30) // aproximación a meses
-
-	return mesesTranscurridos < 3 ? 'New' : null
-}
